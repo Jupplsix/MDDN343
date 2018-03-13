1d23367a50b45f52fc2f40a7298dfe8ff925d63b
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-03-13 18:23:06 +1300

    Uploaded HTML, CSS and Javascript files via upload
    The base lot of files for the functioning blog.

---
+++ script.js
@@ -0,0 +1,57 @@
+var introductionPage = document.querySelector("#introduction-container");
+
+var project1Button = document.querySelector("#project1Button");
+// var project2Button = document.querySelector("#project2Button");
+// var project3Button = document.querySelector("#project3Button");
+
+function resizelanding() {
+	var displayWidth = window.innerWidth;
+	if(displayWidth >= 680) {
+		var displayHeight = window.innerHeight;
+		displayHeight = displayHeight + "px";
+		introductionPage.style.height = displayHeight;
+	};
+};
+
+function resizeLandingOnLoad() {
+	var displayHeight = window.innerHeight;
+	displayHeight = displayHeight + "px";
+	introductionPage.style.height = displayHeight;
+}
+
+function scrollToProject1 () {
+	var project = document.querySelector("#project1");
+	project.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
+
+// function scrollToProject2 () {
+// 	var project = document.querySelector("#project2");
+// 	project.scrollIntoView({
+// 			behavior: "smooth",
+// 			block: "start",
+// 			inline: "nearest"
+// 		});
+// };
+
+// function scrollToProject3 () {
+// 	var project = document.querySelector("#project3");
+// 	project.scrollIntoView({
+// 			behavior: "smooth",
+// 			block: "start",
+// 			inline: "nearest"
+// 		});
+// };
+
+
+
+
+window.addEventListener('resize', resizelanding);
+window.addEventListener('load', resizeLandingOnLoad);
+
+project1Button.addEventListener('click', scrollToProject1);
+// project2Button.addEventListener('click', scrollToProject2);
+// project3Button.addEventListener('click', scrollToProject3);
