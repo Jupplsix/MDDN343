1e5999cd4f972ef51909c6d01f338693736733fb
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-03-21 09:47:59 +1300

    Update JS
    Update JS to scroll to second project post

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var introductionPage = document.querySelector("#introduction-container");
 
 var project1Button = document.querySelector("#project1Button");
-// var project2Button = document.querySelector("#project2Button");
+var project2Button = document.querySelector("#project2Button");
 // var project3Button = document.querySelector("#project3Button");
 
 function resizelanding() {
@@ -28,14 +28,14 @@ function scrollToProject1 () {
 		});
 };
 
-// function scrollToProject2 () {
-// 	var project = document.querySelector("#project2");
-// 	project.scrollIntoView({
-// 			behavior: "smooth",
-// 			block: "start",
-// 			inline: "nearest"
-// 		});
-// };
+function scrollToProject2 () {
+ 	var project = document.querySelector("#project2");
+	project.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
 
 // function scrollToProject3 () {
 // 	var project = document.querySelector("#project3");
@@ -53,5 +53,5 @@ window.addEventListener('resize', resizelanding);
 window.addEventListener('load', resizeLandingOnLoad);
 
 project1Button.addEventListener('click', scrollToProject1);
-// project2Button.addEventListener('click', scrollToProject2);
+project2Button.addEventListener('click', scrollToProject2);
 // project3Button.addEventListener('click', scrollToProject3);
